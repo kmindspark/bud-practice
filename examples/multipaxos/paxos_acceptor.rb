45c672aef435ec0e5b8da522c0acf84b21ab4fe6
Kaushik Shivakumar <kaushikshivakumar@Kaushiks-MBP.localdomain>
2020-01-08 15:48:13 -0800

    acceptor sorta works, but lots of comments saving

--- examples/multipaxos/paxos_acceptor.rb
+++ examples/multipaxos/paxos_acceptor.rb
@@ -27,18 +27,28 @@ class PaxosAcceptor
     existing_id <= cur_prep.notin(max_promise_id, :key=>:key) {|c, pid| true}
     existing_val <= cur_prep.notin(max_accept_val, :key=>:key) {|c, pid| true}
     
-    max_promise_id <+ existing_id { |e| [e.key, 0] }
-    max_accept_val <+ existing_val { |e| [e.key, 0] }
+    #max_promise_id <+ existing_id { |e| [e.key, 0] }
+    #max_accept_val <+ existing_val { |e| [e.key, 0] }
     
-    #promise <~ (prepare * max_promise_id * max_accept_val).combos {|p, mp, ma| [p.val[0], [true, false, mp.val, ma.val, p.val[1]]] if p.val[0] > mp.val and ma.val <= 0}
+    max_promise_id <- (max_promise_id * prepare).pairs {|pid, p| [pid.key, pid.val] if p.val[1] == pid.key}
+    max_promise_id <+ (prepare * max_promise_id).pairs {|p, pid| [p.val[1], p.val[0]] if p.val[1] == pid.key and p.val[0] > pid.val }
+    max_promise_id <+ (prepare * existing_id).pairs {|p, eid| [p.val[1], p.val[0]] }
+
+    promise <~ (prepare * max_promise_id * max_accept_val).combos {|p, mp, ma| [@proposer, [p.val[0], p.val[0] > mpi.val, ma.val > 0, mp.val, ma.val, p.val[1]]] if mp.key == ma.key and p.val[1] == mp.key and p.val[0] > mp.val}
+    promise <~ (prepare * max_promise_id * existing_val).combos {|p, mp, ma| [@proposer, [p.val[0], p.val[0] > mpi.val, false, mp.val, 0, p.val[1]]] if p.val[1] == mp.key}
+    promise <~ (prepare * existing_id * existing_val).combos {|p, mp, ma| [@proposer, [p.val[0], true, false, 0, 0, p.val[1]]] if p.val[1] == ma.key}
+    #promise <~ (prepare * max_promise_id * max_accept_val).pairs {|p, mpi, mav| [@proposer, [p.val[0], false, false, mpi.val, mav.val, p.val[1]]] if p.val[0] <= mpi.val }
+    #promise <~ (prepare * max_promise_id * existing_val).pairs {|p, mpi, mav| [@proposer, [p.val[0], false, false, mpi.val, 0, p.val[1]]] if p.val[0] <= mpi.val}
+
     #promise <~ (prepare * max_promise_id * max_accept_val).combos {|p, mp, ma| [p.val[0], [true, true, mp.val, ma.val, p.val[1]]] if p.val[0] > mp.val and ma.val > 0}
     #promise <~ (prepare * max_promise_id * max_accept_val).combos {|p, mp, ma| [p.val[0], [false, true, mp.val, ma.val, p.val[1]]] if p.val[0] < mp.val}
 
     #stdio <~ prepare.inspected
 
-    #accepted <~ (accept * max_promise_id).pairs {|a, pid| [@proposer, [false, a.val[2], a.val[1]]] if (pid.key == a.key and a.val[0] < pid.val) }
-    #accepted <~ (accept * max_promise_id).pairs {|a, pid| [false, a.val[2], a.val[1]]] if pid.key == a.key and a.val[0] >= pid.val }
-    #max_promise_id <= (accept * max_promise_id).pairs {|a, pid| [a.val[2], a.val[0]] if pid.key == a.key and a.val[0] >= pid.val }
+    accepted <~ (accept * max_promise_id).pairs {|a, pid| [@proposer, [false, a.val[2], a.val[1]]] if (pid.key == a.val[2] and a.val[0] < pid.val) }
+    accepted <~ (accept * max_promise_id).pairs {|a, pid| [@proposer, [false, a.val[2], a.val[1]]] if pid.key == a.val[2] and a.val[0] >= pid.val }
+    max_accept_val <- (max_accept_val * accept * max_promise_id).combos {|mpv, a, pid| [mpv.key, mpv.val] if mpv.key == pid.key and pid.key == a.val[2] and a.val[0] >= pid.val }
+    max_accept_val <+ (accept * max_promise_id).pairs {|a, pid| [a.val[2], a.val[0]] if pid.key == a.val[2] and a.val[0] >= pid.val }
 
     #accepted <~ accept {|a| [@proposer, check_accept(a.val)]}
   end
