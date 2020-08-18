1f5645a3f27612c7e871ecfc5e02e2fe2b7cb926
Kaushik Shivakumar <kaushiks@berkeley.edu>
2020-08-17 18:07:02 -0700

    stuff

--- cloudburst_func.py
+++ cloudburst_func.py
@@ -5,8 +5,8 @@ import argparse
 
 
 local = False
-AWS_FUNCTION_ELB = 'a761b9343137342de987258bdcdda6df-671553808.us-east-1.elb.amazonaws.com'
-MY_IP = 'ec2-54-146-61-171'#'18.209.27.93'
+AWS_FUNCTION_ELB = 'a591fc1389d974046a76910d7644370e-176098095.us-east-1.elb.amazonaws.com'
+MY_IP = 'ec2-54-210-86-239'#'18.209.27.93'
 
 from cloudburst.client.client import CloudburstConnection
 cloudburst = CloudburstConnection(AWS_FUNCTION_ELB, MY_IP, local=local)
@@ -32,14 +32,34 @@ def proposer(user_lib, a):
     subprocess.call(["ruby", "/bud-practice/examples/multipaxos_cloudburst/paxos_proposer.rb", "0", user_lib.get('proposer_ip') + ":12345", user_lib.get('acceptor_ip') + ":12346"])
     return 100
 
-def acceptor(user_lib, a):
+def acceptor_1(user_lib, a):
     ip = str(os.popen("ifconfig eth0 | grep 'inet' | grep -v inet6 | sed -e 's/^[ \t]*//' | cut -d' ' -f2").read())[:-1]
     user_lib.put('acceptor_ip', ip)
     print("INSIDE ACCEPTOR", ip)
     while (not (user_lib.get('proposer_ip') and user_lib.get('client_ip') and user_lib.get('acceptor_ip'))):
         time.sleep(1)
     time.sleep(5)
-    subprocess.call(["ruby", "/bud-practice/examples/multipaxos_cloudburst/paxos_acceptor.rb", user_lib.get('acceptor_ip') + ":1234" + str(a), user_lib.get('proposer_ip') + ":12345"])
+    subprocess.call(["ruby", "/bud-practice/examples/multipaxos_cloudburst/paxos_acceptor.rb", ip + ":1234" + str(a), user_lib.get('proposer_ip') + ":12345"])
+    return 100
+
+def acceptor_2(user_lib, a):
+    ip = str(os.popen("ifconfig eth0 | grep 'inet' | grep -v inet6 | sed -e 's/^[ \t]*//' | cut -d' ' -f2").read())[:-1]
+    user_lib.put('acceptor_ip', ip)
+    print("INSIDE ACCEPTOR", ip)
+    while (not (user_lib.get('proposer_ip') and user_lib.get('client_ip') and user_lib.get('acceptor_ip'))):
+        time.sleep(1)
+    time.sleep(5)
+    subprocess.call(["ruby", "/bud-practice/examples/multipaxos_cloudburst/paxos_acceptor.rb", ip + ":1234" + str(a), user_lib.get('proposer_ip') + ":12345"])
+    return 100
+
+def acceptor_3(user_lib, a):
+    ip = str(os.popen("ifconfig eth0 | grep 'inet' | grep -v inet6 | sed -e 's/^[ \t]*//' | cut -d' ' -f2").read())[:-1]
+    user_lib.put('acceptor_ip', ip)
+    print("INSIDE ACCEPTOR", ip)
+    while (not (user_lib.get('proposer_ip') and user_lib.get('client_ip') and user_lib.get('acceptor_ip'))):
+        time.sleep(1)
+    time.sleep(5)
+    subprocess.call(["ruby", "/bud-practice/examples/multipaxos_cloudburst/paxos_acceptor.rb", ip + ":1234" + str(a), user_lib.get('proposer_ip') + ":12345"])
     return 100
 
 parser = argparse.ArgumentParser(description='Set nums of each.')
@@ -51,7 +71,12 @@ args = parser.parse_args()
 
 cloud_client = cloudburst.register(client, 'client')
 cloud_proposer = cloudburst.register(proposer, 'proposer')
-cloud_acceptor = cloudburst.register(acceptor, 'acceptor')
+
+for i in range(args.a):
+    cloud_acceptor_1 = cloudburst.register(acceptor_1, 'acceptor-1')
+    cloud_acceptor_2 = cloudburst.register(acceptor_2, 'acceptor-2')
+    cloud_acceptor_3 = cloudburst.register(acceptor_3, 'acceptor-3')
+
 print("REGISTERED FUNCTIONS")
 
 cloudburst.register_dag('dag', ['client'], [])
@@ -62,14 +87,14 @@ cloudburst.register_dag('dag2', ['proposer'], [])
 print("Registered second dag")
 print(cloudburst.call_dag('dag2', { 'proposer': [2] }, direct_response=False))
 
-cloudburst.register_dag('dag3', ['acceptor'], [])
+cloudburst.register_dag('dag3', ['acceptor-1'], [])
 print("Registered third dag")
-print(cloudburst.call_dag('dag3', { 'acceptor': [1] }, direct_response=False))
+print(cloudburst.call_dag('dag3', { 'acceptor-1': [1] }, direct_response=False))
 
-cloudburst.register_dag('dag4', ['acceptor'], [])
+cloudburst.register_dag('dag4', ['acceptor-2'], [])
 print("Registered fourth dag")
-print(cloudburst.call_dag('dag4', { 'acceptor': [2] }, direct_response=False))
+print(cloudburst.call_dag('dag4', { 'acceptor-2': [2] }, direct_response=False))
 
-cloudburst.register_dag('dag5', ['acceptor'], [])
+cloudburst.register_dag('dag5', ['acceptor-3'], [])
 print("Registered fifth dag")
-print(cloudburst.call_dag('dag5', { 'acceptor': [3] }, direct_response=False))
+print(cloudburst.call_dag('dag5', { 'acceptor-3': [3] }, direct_response=False))
